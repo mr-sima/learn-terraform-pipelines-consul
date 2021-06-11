a18e5b53a0c380cc01371510dac2530139839e78
mr-sima <77833765+mr-sima@users.noreply.github.com>
2021-06-11 10:21:44 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
     organization = "infrastructure-pipelines-workshop"
 
     workspaces {
-      name = "john-d-consul"
+      name = "simon-m-consul"
     }
   }
   required_providers {
